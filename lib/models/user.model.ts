6103f83e6e64cbc8ff9c72378222aabdd05f315d
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-03-07 17:10:03 +0600

    designed User model

---
+++ lib/models/user.model.ts
@@ -0,0 +1,47 @@
+import { model, models, Schema } from 'mongoose';
+
+interface IUser {
+  _id?: string;
+  clerkId: string;
+  username?: string;
+  name: string;
+  email: string;
+  role: 'vandy' | 'foodie' | 'admin';
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const UserSchema = new Schema(
+  {
+    clerkId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    username: {
+      type: String,
+      unique: true,
+      sparse: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      enum: ['vandy', 'foodie', 'admin'],
+      default: 'foodie',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = models.User || model<IUser>('User', UserSchema);
+export default User;
