d50fa2da8b4c89767abe562f2a821991b144b790
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-03-07 22:54:48 +0600

    added Navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import Navbar from '@/components/Navbar';
+
 export default function Home() {
   return (
     <>
-      <header>
-        <nav>Navbar</nav>
+      <header className="p-4 max-w-7xl mx-auto">
+        <Navbar />
       </header>
-      <main>Main</main>
+      <main className="min-h-screen p-4 max-w-7xl mx-auto">Main</main>
       <footer>Footer</footer>
     </>
   );
