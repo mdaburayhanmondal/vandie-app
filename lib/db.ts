126767ac4574c061400f7830cf8f8f6004454e57
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-03-07 17:09:45 +0600

    connected db

---
+++ lib/db.ts
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.DB_URI as string;
+
+if (!MONGODB_URI) {
+  throw new Error('MongoDB URI is missing.');
+}
+
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+declare global {
+  var mongoose: MongooseCache;
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function connectToDatabase() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
