cefa94a502d7228ac077f64ef22331b49677e5a9
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-03-07 22:54:05 +0600

    added desktop and mobile menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import { syncUser } from '@/lib/actions/user.actions';
+import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs';
+import Link from 'next/link';
+import DesktopMenu from './DesktopMenu';
+import MobileMenu from './MobileMenu';
 
-const Navbar = () => {
-  return <div>Navbar</div>;
+const Navbar = async () => {
+  const dbUser = await syncUser();
+  console.log(dbUser?.role);
+
+  return (
+    <nav className="flex items-center justify-between">
+      <Link href={'/'} className="text-2xl font-semibold italic">
+        Vandie
+      </Link>
+
+      {/* desktop menus */}
+      <DesktopMenu dbUser={dbUser} />
+
+      {/* mobile menus */}
+      <MobileMenu dbUser={dbUser} />
+
+      <div className="flex gap-x-2">
+        <Show when="signed-out">
+          <SignInButton />
+          <SignUpButton>
+            <button className="bg-[#5732eb] hover:bg-[#2f09c6] text-white rounded-md font-medium text-sm sm:text-base px-3 py-1 cursor-pointer">
+              Sign Up
+            </button>
+          </SignUpButton>
+        </Show>
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
