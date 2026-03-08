62275b201a5a01e1da9bcf0a05a82c43c00483bd
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-03-08 19:34:20 +0600

    better structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/app/globals.css';
+import Navbar from '@/components/Navbar';
 import { ClerkProvider } from '@clerk/nextjs';
 
 export default function RootLayout({
@@ -8,9 +9,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        <ClerkProvider>{children}</ClerkProvider>
-      </body>
+      <ClerkProvider>
+        <body>
+          <header className="max-w-7xl mx-auto">
+            <Navbar />
+          </header>
+          <main className="min-h-screen max-w-7xl mx-auto">{children}</main>
+          <footer className="max-w-7xl mx-auto">Footer</footer>
+        </body>
+      </ClerkProvider>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,3 @@
-import Navbar from '@/components/Navbar';
-
 export default function Home() {
-  return (
-    <>
-      <header className=" max-w-7xl mx-auto">
-        <Navbar />
-      </header>
-      <main className="min-h-screen max-w-7xl mx-auto">Main</main>
-      <footer>Footer</footer>
-    </>
-  );
+  return <></>;
 }
