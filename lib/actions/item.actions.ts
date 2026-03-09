434356bdc097548ae1ffb666566d16c82535881c
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-03-09 21:17:02 +0600

    add item action added

---
+++ lib/actions/item.actions.ts
@@ -0,0 +1,50 @@
+'use server';
+
+import { auth } from '@clerk/nextjs/server';
+import { connectToDatabase } from '../db';
+import Item from '../models/item.model';
+import User from '../models/user.model';
+import { revalidatePath } from 'next/cache';
+
+export async function addItem(formData: FormData) {
+  try {
+    const { userId } = await auth();
+    if (!userId) throw new Error('Unauthorized!');
+
+    await connectToDatabase();
+
+    const user = await User.findOne({ clerkId: userId });
+    if (user?.role !== 'vandy') {
+      throw new Error('Only Vandies can add menu items.');
+    }
+
+    const name = formData.get('name') as string;
+    const price = Number(formData.get('price'));
+    const category = formData.get('category') as string;
+    const description = formData.get('description') as string;
+    const imageUrl = formData.get('image') as string;
+
+    const isAvailable = formData.get('isAvailable') === 'true';
+
+    if (!name || isNaN(price) || !category) {
+      throw new Error('Please fill all required fields.');
+    }
+
+    await Item.create({
+      ownerId: userId,
+      name,
+      price,
+      category,
+      description,
+      isAvailable,
+      image: imageUrl,
+    });
+
+    revalidatePath('/vandy-dashboard');
+
+    return { success: true };
+  } catch (err) {
+    console.error('Error adding item:', err);
+    return { success: false };
+  }
+}
