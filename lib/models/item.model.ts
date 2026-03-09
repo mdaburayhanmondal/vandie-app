d3b9d7cbfacddc8d9aa456b4b685c044eb692f99
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-03-09 21:00:12 +0600

    added Item model

---
+++ lib/models/item.model.ts
@@ -0,0 +1,56 @@
+import { model, models, Schema, Document } from 'mongoose';
+
+export interface IItem extends Document {
+  ownerId: string;
+  name: string;
+  price: number;
+  category: string;
+  description?: string;
+  isAvailable: boolean;
+  image?: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const ItemSchema = new Schema(
+  {
+    ownerId: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: ['Snacks', 'Meals', 'Drinks', 'Desserts', 'Others'],
+      default: 'Others',
+    },
+    description: {
+      type: String,
+      maxLength: 150,
+    },
+    isAvailable: {
+      type: Boolean,
+      default: true,
+    },
+    image: {
+      type: String,
+      default: '',
+    },
+  },
+  { timestamps: true },
+);
+
+const Item = models.Item || model<IItem>('Item', ItemSchema);
+
+export default Item;
