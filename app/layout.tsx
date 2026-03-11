3a8620819cc7c454191fa7df8cb071390cb137d1
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-03-12 04:12:45 +0600

    wrapped with cart provider and added floating cart summery component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import '@/app/globals.css';
+import FloatingCart from '@/components/FloatingCart';
 import Navbar from '@/components/Navbar';
+import { CartProvider } from '@/contexts/cartContext';
 import { ClerkProvider } from '@clerk/nextjs';
 
 export default function RootLayout({
@@ -10,13 +12,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <ClerkProvider>
-        <body>
-          <header className="max-w-7xl mx-auto p-4">
-            <Navbar />
-          </header>
-          <main className="min-h-screen max-w-7xl mx-auto p-4">{children}</main>
-          <footer className="max-w-7xl mx-auto">Footer</footer>
-        </body>
+        <CartProvider>
+          <body>
+            <header className="max-w-7xl mx-auto p-4">
+              <Navbar />
+            </header>
+            <main className="min-h-screen max-w-7xl mx-auto p-4">
+              {children}
+            </main>
+            <FloatingCart />
+            <footer className="max-w-7xl mx-auto">Footer</footer>
+          </body>
+        </CartProvider>
       </ClerkProvider>
     </html>
   );
